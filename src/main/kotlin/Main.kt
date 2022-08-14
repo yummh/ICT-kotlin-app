52121ad8f19fbaeeef1328f2188c633d143cbf25
ymh <dbalsgml207@gmail.com>
2022-08-14 19:17:40 +0900

    실습 3-1. main 함수 구현을 클래스 함수로 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,11 @@
+import screen.AppHome
+
+/*
+step1. main 함수 -> 클래스 함수
+step2. 화면별 클래스 분리
+step3. 선택한 카테고리 상품 목록 표시
+*/
 fun main(args: Array<String>) {
-    println("안녕하세요, ICT APP에 오신 것을 환영합니다!")
-    println("앱을 계속 사용하시려면 이름을 입력해주세요 : ")
-
-    val name = readLine()
-    println(
-        """
-            반갑습니다, $name 님 원하시는 카테고리를 입력해주세요.
-        """.trimIndent())
-
-    /*
-        1. 상품 카테고리 표기
-        2. 사용자 입력 받기
-        3. 사용자가 기대하는 값을 입력하지 않는 경우에 대한 처리
-    */
-    val categories = arrayOf("날씨", "복지", "챗봇")
-    for (category in categories) {
-        println(category)
-    }
-    println("메인화면으로 이동하려면 #을 입력해주세요")
-
-    var selectedCategory = readLine()
-    while (selectedCategory.isNullOrBlank()) {
-        println("원하는 카테고리를 입력해주세요")
-        selectedCategory = readLine()
-    }
-
-    if (selectedCategory == "#") {
-        // TODO 1. 메인화면 이동
-    } else {
-        // TODO 2. 카테고리 목록 보여주기
-        // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
-    }
+    val appHome = AppHome()
+    appHome.start()
 }
---
+++ src/main/kotlin/screen/AppHome.kt
@@ -0,0 +1,46 @@
+package screen
+
+class AppHome {
+    fun start(){
+        showWelcomeMessage()
+        showCategories()
+    }
+
+    private fun showWelcomeMessage() {
+        println("안녕하세요, ICT APP에 오신 것을 환영합니다!")
+        println("앱을 계속 사용하시려면 이름을 입력해주세요 : ")
+
+        val name = readLine()
+        println(
+            """
+                반갑습니다, $name 님 원하시는 카테고리를 입력해주세요.
+            """.trimIndent()
+        )
+    }
+
+    private fun showCategories() {
+        /*
+            1. 상품 카테고리 표기
+            2. 사용자 입력 받기
+            3. 사용자가 기대하는 값을 입력하지 않는 경우에 대한 처리
+        */
+        val categories = arrayOf("날씨", "복지", "챗봇")
+        for (category in categories) {
+            println(category)
+        }
+        println("메인화면으로 이동하려면 #을 입력해주세요")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank()) {
+            println("원하는 카테고리를 입력해주세요")
+            selectedCategory = readLine()
+        }
+
+        if (selectedCategory == "#") {
+            // TODO 1. 메인화면 이동
+        } else {
+            // TODO 2. 카테고리 목록 보여주기
+            // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
+        }
+    }
+}
